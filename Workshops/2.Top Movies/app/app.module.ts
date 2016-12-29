df8bad779aaeda70c7c601b8395f8f08725b523d
IvayloPaskalev <ivaylo.paskalew@gmail.com>
2016-12-29 23:08:28 +0200

    Reordering

--- Workshops/2.Top Movies/app/app.module.ts
+++ Workshops/2.Top Movies/app/app.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 
-import { SortPipe } from '../core/movie/pipes/movieSort';
-import { FilterPipe } from '../core/movie/pipes/filterMovies';
+import { SortPipe } from './core/movie/pipes/movieSort';
+import { FilterPipe } from './core/movie/pipes/filterMovies';
 
 import { AppComponent } from './app.component';
-import { MoviesListComponent } from '../core/movie/movies-list.component';
-import { MovieItemComponent } from '../core/movie/movie-item.component';
+import { MoviesListComponent } from './core/movie/movies-list.component';
+import { MovieItemComponent } from './core/movie/movie-item.component';
 
 import { Ng2BootstrapModule } from 'ng2-bootstrap';
 
-import { MovieService } from '../core/movie/services/movies.service';
+import { MovieService } from './core/movie/services/movies.service';
 
 @NgModule({
 	imports: [BrowserModule, HttpModule, Ng2BootstrapModule],
